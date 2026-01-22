beeb993436d517975bb56edf9a8c33560d6aa0be
shamooo123 <105862503+shamooo123@users.noreply.github.com>
2026-01-22 13:12:23 +0000

    Update product-data.js

--- product-data.js
+++ product-data.js
@@ -1,18 +1,15 @@
 // product-data.js
-// This is your "Database". The Store Page reads this to show items.
+// This acts as the database for your Store Page.
 
 window.MBL_PRODUCTS = [
+    // --- FEATURED ---
     {
         id: "valentines-set",
         title: "Valentines Cookie Cutter Set (3pc)",
         price: "£12.99",
         category: "Cookie Cutters",
         image: "./images/valentines-set-main.jpg",
-        
-        // --- THIS IS WHERE YOUR EBAY LINK GOES ---
-        link: "https://www.ebay.co.uk/itm/206015245594", 
-        // ----------------------------------------
-        
+        link: "https://www.ebay.co.uk/itm/206015245594",
         badge: "SEASONAL"
     },
     {
@@ -21,9 +18,95 @@ window.MBL_PRODUCTS = [
         price: "£12.99",
         category: "Cookie Cutters",
         image: "./images/vehicle-set-main.jpg",
-        link: "product.html", // You can link to your own detail page OR eBay
+        link: "https://www.ebay.co.uk/itm/206015223984", // Updated with your specific link
         badge: "BESTSELLER"
     },
+
+    // --- NEW COOKIE CUTTERS ---
+    {
+        id: "dinosaur-set",
+        title: "Dinosaur Cookie Cutter Set",
+        price: "£12.99",
+        category: "Cookie Cutters",
+        image: "./images/dinosaur-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/206015252676"
+    },
+    {
+        id: "space-set",
+        title: "Space Cookie Cutter Set",
+        price: "£12.99",
+        category: "Cookie Cutters",
+        image: "./images/space-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/206015268484"
+    },
+    {
+        id: "unicorn-set",
+        title: "Unicorn Cookie Cutter Set",
+        price: "£12.99",
+        category: "Cookie Cutters",
+        image: "./images/unicorn-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/206015272651"
+    },
+    {
+        id: "farm-set",
+        title: "Farm Animal Cookie Cutter Set",
+        price: "£12.99",
+        category: "Cookie Cutters",
+        image: "./images/farm-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/206015299632"
+    },
+    {
+        id: "easter-set",
+        title: "Easter Cookie Cutter Set",
+        price: "£12.99",
+        category: "Cookie Cutters",
+        image: "./images/easter-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/205735219896"
+    },
+    {
+        id: "construction-set",
+        title: "Construction Cookie Cutter Set",
+        price: "£9.99",
+        category: "Cookie Cutters",
+        image: "./images/construction-set.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/206015063487"
+    },
+
+    // --- ORGANIZERS / GRIDFINITY ---
+    {
+        id: "grid-baseplate",
+        title: "Gridfinity Baseplate",
+        price: "£8.99",
+        category: "Organizers",
+        image: "./images/grid-baseplate.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/205889670143"
+    },
+    {
+        id: "grid-bins-1x1",
+        title: "Gridfinity Bins (1x1)",
+        price: "£4.99",
+        category: "Organizers",
+        image: "./images/grid-bins-1x1.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/205889723976"
+    },
+    {
+        id: "grid-bins-1x2",
+        title: "Gridfinity Bins (1x2)",
+        price: "£5.99",
+        category: "Organizers",
+        image: "./images/grid-bins-1x2.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/205889750360"
+    },
+    {
+        id: "grid-bins-2x2",
+        title: "Gridfinity Bins (2x2)",
+        price: "£6.99",
+        category: "Organizers",
+        image: "./images/grid-bins-2x2.jpg", // You need to add this image
+        link: "https://www.ebay.co.uk/itm/205889760737"
+    },
+
+    // --- OTHER ---
     {
         id: "custom-design",
         title: "Custom Design Request",
